1aa7ff36b71993e48300d4f01d93850658cc3667
unknown <Sonali Keshri>
2024-07-19 16:21:10 +0530

    update in social icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
     <div className='header_socials'>
       <a href="https://www.linkedin.com/in/sonali-keshri-322875225" target="__blank"><BsLinkedin/></a>
       <a href="https://github.com/Sonali-keshri" target="__blank"><BsGithub/></a>
-      <a href="https://www.instagram.com/mahi.ra45/" target="__blank"><GrInstagram/></a>
+      
     </div>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,12 +4,14 @@ import project1 from "../../assests/crudapp.png";
 import project2 from "../../assests/chatapp_home.png";
 import project3 from '../../assests/hackthon_Submission.png'
 import project4 from "../../assests/movie_app.png";
-import project5 from "../../assests/weatherapp.png";
-import project6 from "../../assests/product_cart.png";
-import project7 from "../../assests/text-utilizer.png";
+import project5 from "../../assests/ai_certs.jpeg";
+import project6 from "../../assests/weatherapp.png";
+import project7 from "../../assests/product_cart.png";
+import project8 from "../../assests/text-utilizer.png";
 // import project5 from "../../assests/todoapp.png";
-import project8 from "../../assests/landingPage.png";
-import project9 from "../../assests/newsapp.png";
+import project9 from "../../assests/landingPage.png";
+import project10 from "../../assests/newsapp.png";
+import project11 from "../../assests/productPage.jpeg";
 
 const Portfolio = () => {
   const data = [
@@ -37,46 +39,59 @@ const Portfolio = () => {
     {
       id: 4,
       image: project4,
-      title: "Weather App we can see the temparature of any place",
+      title: "Find any hollywood movies or TV shows ",
       github: "https://github.com/Sonali-keshri/Movie_APP",
       demo: "https://main--magical-show-movieapp.netlify.app/",
       
     },
     {
       id: 5,
-      image: project4,
-      title: "Weather App we can see the temparature of any place",
-      github: "https://github.com/Sonali-keshri/weatherApp",
-      demo: "https://snowaweatherapp.netlify.app",
-      
+      image: project5,
+      title: "Website deisgned from scratched using HTML, CSS, JAvascript and Swiperjs",
+      github: "https://github.com/Sonali-keshri/AI_certs",
+      demo: "https://ai-certs-website.netlify.app/", 
     },
     {
       id: 6,
-      image: project5,
+      image: project6,
+      title: "Weather App we can see the temparature of any place",
+      github: "https://github.com/Sonali-keshri/weatherApp",
+      demo: "https://snowaweatherapp.netlify.app", 
+    },
+    {
+      id: 7,
+      image: project7,
       title: "Product cart App to place an order",
       github: "https://github.com/Sonali-keshri/Product_Cart_APP",
       demo: "https://sonali-productcart.netlify.app/",
     },
     {
-      id: 7,
-      image: project6,
+      id: 8,
+      image: project8,
       title: "Text-Changer to manipulate the text",
       github: "https://github.com/Sonali-keshri/Text-changer",
       demo: "https://text-changerapp.netlify.app",
     },
     {
-      id: 8,
-      image: project7,
+      id: 9,
+      image: project9,
       title: "Responisive Landing Page using Bootstrap",
       github: "https://github.com/Sonali-keshri/LandingPage",
       demo: "https://intelthinkpage.netlify.app/",
     },
     {
-      id: 9,
-      image: project8,
+      id: 10,
+      image: project10,
       title: "Daily News App by using fetchApi And API used here from NewsApi",
       github: "https://github.com/Sonali-keshri/NewsApp",
       demo:"",
+    },
+    {
+      id: 11,
+      image: project11,
+      title: "Simple website with product card details displaying from api",
+      github: "https://github.com/Sonali-keshri/Gobananas",
+      demo:"https://gobananas-cart.netlify.app/",
     },
   ];
   return (
